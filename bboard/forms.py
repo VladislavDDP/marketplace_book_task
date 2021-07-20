10e1d3a111e5b6418155db831b4f1b8ff1b0b86d
ilarionov <bombardire54@gmail.com>
2021-07-20 11:45:30 +0300

    Update form input. Add placeholders

--- bboard/forms.py
+++ bboard/forms.py
@@ -1,7 +1,17 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Bb
 
 class BbForm(ModelForm):
     class Meta:
         model = Bb
         fields = '__all__'
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Название'}),
+            'rubric': forms.Select(attrs={'class': 'form-control', 'placeholder': 'Рубрика'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Описание'}),
+            'price': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Цена'}),
+            'date': forms.DateInput(attrs={'class': 'form-control', 'placeholder': 'Дата'}),
+            'city': forms.Select(attrs={'class': 'form-control', 'placeholder': 'Город'})
+        }
